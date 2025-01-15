9dbe1a5f405f2e2e82d934c5eda646f790329b7b
Tony Gadaleta <anthonygadaleta@gmail.com>
2025-01-15 15:38:17 -0500

    created homepage backbone

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <>
+        <div className='home-container'>
+            <section className='hero'>
+                <div className='hero-content'>
+                    <h1 className='hero-title'>Welcome To My Portfolio</h1>
+                    <p className='hero-subtitle'>Heya! I'm Tony, a Software Engineer with a passion for learning and teaching.</p>
+                    <div className='hero-buttons'>
+                        <a href='/projects' className='projects-btn'>View My Portfolio</a>
+                        <a href='/about' className='about-btn'>About Me</a>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </>
+  )
+}
+
+export default HomePage
