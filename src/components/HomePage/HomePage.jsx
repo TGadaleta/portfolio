3df2d04481ef92f5be064922b0f597dc0ea3fe00
Tony Gadaleta <anthonygadaleta@gmail.com>
2025-02-02 20:14:52 -0500

    refactor GameCupidPage layout and update HomePage links; enhance global styles

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -10,8 +10,8 @@ const HomePage = () => {
                     <h1 className='hero-title'>Welcome To My Portfolio</h1>
                     <p className='hero-subtitle'>Heya! I'm Tony, a Software Engineer with a passion for learning and teaching.</p>
                     <div className='hero-buttons'>
-                        <a href='/projects' className='projects-btn'>View My Portfolio</a>
-                        <a href='/about' className='about-btn'>About Me</a>
+                        <a href='/gamecupid' className='projects-btn'>View My Portfolio</a>
+                        <a href='/contact' className='about-btn'>Contact</a>
                     </div>
                 </div>
             </section>
