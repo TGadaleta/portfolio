d2dfae567e5d35fb4ab3c2dcb6b1f70770ecbb08
Tony Gadaleta <anthonygadaleta@gmail.com>
2025-02-03 13:11:07 -0500

    update HomePage content and enhance styles; improve hero section text and add max-width to bio container

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -7,8 +7,8 @@ const HomePage = () => {
         <div className='home-container'>
             <section className='hero'>
                 <div className='hero-content'>
-                    <h1 className='hero-title'>Welcome To My Portfolio</h1>
-                    <p className='hero-subtitle'>Heya! I'm Tony, a Software Engineer with a passion for learning and teaching.</p>
+                    <h1 className='hero-title'>Welcome To My Portfolio!</h1>
+                    <p className='hero-subtitle'>Heya! I'm Tony Gadaleta, a Software Engineer with a passion for learning, teaching, and gaming.</p>
                     <div className='hero-buttons'>
                         <a href='/gamecupid' className='projects-btn'>View My Portfolio</a>
                         <a href='/contact' className='about-btn'>Contact</a>
